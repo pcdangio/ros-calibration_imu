79caa9e24f43d0f4791131051ca9f241c6d52b3e
Paul D'Angio <pcdangio@gmail.com>
2020-10-06 23:08:17 -0400

    magnetometer: switched form over to data interface

--- src/magnetometer/gui/fmain.cpp
+++ src/magnetometer/gui/fmain.cpp
@@ -8,10 +8,10 @@ fmain::fmain(QWidget *parent)
     ui->setupUi(this);
 
     // Set up node handle.
-    fmain::m_node = new ros::NodeHandle("~");
+    fmain::m_node = new ros::NodeHandle();
 
-    // Read parameters.
-    fmain::p_max_data_rate = fmain::m_node->param<double>("max_data_rate", 10.0);
+    // Create data interface instance.
+    fmain::m_data_interface = std::make_shared<magnetometer::data_interface>();
 
     // Initialize state.
     fmain::m_state = fmain::state_t::IDLE;
@@ -30,12 +30,6 @@ fmain::fmain(QWidget *parent)
 
 fmain::~fmain()
 {
-    // Cancel any running collection.
-    fmain::stop_collection();
-
-    // Clear any collection.
-    fmain::clear_collection();
-
     // Clear charts.
     fmain::clear_charts();
 
@@ -62,12 +56,17 @@ void fmain::ros_spin()
 
 void fmain::on_button_start_collection_clicked()
 {
-    fmain::start_collection();
+    // Start the data interface subscriber.
+    fmain::m_data_interface->start_subscriber();
 }
-
 void fmain::on_button_stop_collection_clicked()
 {
-    fmain::stop_collection();
+    // Stop the data interface subscribers.
+    fmain::m_data_interface->stop_subscriber();
+}
+void fmain::on_button_clear_collection_clicked()
+{
+    fmain::m_data_interface->clear_data();
 }
 
 void fmain::on_combobox_charts_currentIndexChanged(int index)
@@ -79,60 +78,7 @@ void fmain::on_combobox_charts_currentIndexChanged(int index)
     }
 }
 
-void fmain::on_button_clear_collection_clicked()
-{
-    fmain::clear_collection();
-}
 
-
-void fmain::start_collection()
-{
-    if(fmain::m_state == fmain::state_t::IDLE)
-    {
-        // Enable point timer.
-        fmain::m_point_timer.start();
-
-        // Subscribe to magnetometer data stream.
-        ros::NodeHandle public_handle;
-        fmain::m_subscriber = public_handle.subscribe("/imu/magnetometer", 100, &fmain::subscriber, this);
-
-        // Update state.
-        fmain::m_state = fmain::state_t::COLLECTION;
-    }
-}
-void fmain::stop_collection()
-{
-    if(fmain::m_state == fmain::state_t::COLLECTION)
-    {
-        // Close down subscriber.
-        fmain::m_subscriber.shutdown();
-
-        // Stop point timer.
-        fmain::m_point_timer.invalidate();
-
-        // Update state.
-        fmain::m_state = fmain::state_t::IDLE;
-    }
-}
-void fmain::clear_collection()
-{
-    if(fmain::m_state == fmain::state_t::IDLE || fmain::m_state == fmain::state_t::COLLECTION)
-    {
-        // Clear deque.
-        fmain::m_points.clear();
-
-        // Update charts.
-        fmain::update_charts();
-
-        // Clear form's point counter.
-        fmain::ui->lineedit_n_collection_points->setText(0);
-    }
-    // Clean up points.
-    for(auto point = fmain::m_points.cbegin(); point != fmain::m_points.cend(); ++point)
-    {
-        delete (*point);
-    }
-}
 
 void fmain::start_fit()
 {
@@ -230,180 +176,152 @@ void fmain::clear_charts()
 }
 void fmain::update_charts()
 {
-    // Iterate through all points to set up xy, xz, and yz vectors.
-    QVector<QPointF> points_xy;
-    QVector<QPointF> points_xz;
-    QVector<QPointF> points_yz;
-    double x_min = std::numeric_limits<double>::max();
-    double x_max = -std::numeric_limits<double>::max();
-    double y_min = std::numeric_limits<double>::max();
-    double y_max = -std::numeric_limits<double>::max();
-    double z_min = std::numeric_limits<double>::max();
-    double z_max = -std::numeric_limits<double>::max();
-    for(auto point_entry = fmain::m_points.cbegin(); point_entry != fmain::m_points.cend(); ++point_entry)
-    {
-        fmain::point_t* point = *point_entry;
+//    // Iterate through all points to set up xy, xz, and yz vectors.
+//    QVector<QPointF> points_xy;
+//    QVector<QPointF> points_xz;
+//    QVector<QPointF> points_yz;
+//    double x_min = std::numeric_limits<double>::max();
+//    double x_max = -std::numeric_limits<double>::max();
+//    double y_min = std::numeric_limits<double>::max();
+//    double y_max = -std::numeric_limits<double>::max();
+//    double z_min = std::numeric_limits<double>::max();
+//    double z_max = -std::numeric_limits<double>::max();
+//    for(auto point_entry = fmain::m_points.cbegin(); point_entry != fmain::m_points.cend(); ++point_entry)
+//    {
+//        fmain::point_t* point = *point_entry;
 
-        // Add points to vector.
-        points_xy.append(QPointF(point->x, point->y));
-        points_xz.append(QPointF(point->x, point->z));
-        points_yz.append(QPointF(point->y, point->z));
+//        // Add points to vector.
+//        points_xy.append(QPointF(point->x, point->y));
+//        points_xz.append(QPointF(point->x, point->z));
+//        points_yz.append(QPointF(point->y, point->z));
 
-        // Calculate data ranges.
-        // X
-        if(point->x < x_min)
-        {
-            x_min = point->x;
-        }
-        if(point->x > x_max)
-        {
-            x_max = point->x;
-        }
-        // Y
-        if(point->y < y_min)
-        {
-            y_min = point->y;
-        }
-        if(point->y > y_max)
-        {
-            y_max = point->y;
-        }
-        // Z
-        if(point->z < z_min)
-        {
-            z_min = point->z;
-        }
-        if(point->z > z_max)
-        {
-            z_max = point->z;
-        }
-    }
+//        // Calculate data ranges.
+//        // X
+//        if(point->x < x_min)
+//        {
+//            x_min = point->x;
+//        }
+//        if(point->x > x_max)
+//        {
+//            x_max = point->x;
+//        }
+//        // Y
+//        if(point->y < y_min)
+//        {
+//            y_min = point->y;
+//        }
+//        if(point->y > y_max)
+//        {
+//            y_max = point->y;
+//        }
+//        // Z
+//        if(point->z < z_min)
+//        {
+//            z_min = point->z;
+//        }
+//        if(point->z > z_max)
+//        {
+//            z_max = point->z;
+//        }
+//    }
 
-    // Update collection series.
-    fmain::m_series_collections[fmain::chart_t::XY]->replace(points_xy);
-    fmain::m_series_collections[fmain::chart_t::XZ]->replace(points_xz);
-    fmain::m_series_collections[fmain::chart_t::YZ]->replace(points_yz);
+//    // Update collection series.
+//    fmain::m_series_collections[fmain::chart_t::XY]->replace(points_xy);
+//    fmain::m_series_collections[fmain::chart_t::XZ]->replace(points_xz);
+//    fmain::m_series_collections[fmain::chart_t::YZ]->replace(points_yz);
 
-    // Update current point series.
-    if(!fmain::m_points.empty())
-    {
-        // Create vectors.
-        QVector<QPointF> points_xy_current;
-        QVector<QPointF> points_xz_current;
-        QVector<QPointF> points_yz_current;
-        // Populate vectors.
-        auto current_point = fmain::m_points.back();
-        points_xy_current.append(QPointF(current_point->x, current_point->y));
-        points_xz_current.append(QPointF(current_point->x, current_point->z));
-        points_yz_current.append(QPointF(current_point->y, current_point->z));
-        // Add vectors to series.
-        fmain::m_series_current_position[fmain::chart_t::XY]->replace(points_xy_current);
-        fmain::m_series_current_position[fmain::chart_t::XZ]->replace(points_xz_current);
-        fmain::m_series_current_position[fmain::chart_t::YZ]->replace(points_yz_current);
-    }
-    else
-    {
-        fmain::m_series_current_position[fmain::chart_t::XY]->clear();
-        fmain::m_series_current_position[fmain::chart_t::XZ]->clear();
-        fmain::m_series_current_position[fmain::chart_t::YZ]->clear();
-    }
+//    // Update current point series.
+//    if(!fmain::m_points.empty())
+//    {
+//        // Create vectors.
+//        QVector<QPointF> points_xy_current;
+//        QVector<QPointF> points_xz_current;
+//        QVector<QPointF> points_yz_current;
+//        // Populate vectors.
+//        auto current_point = fmain::m_points.back();
+//        points_xy_current.append(QPointF(current_point->x, current_point->y));
+//        points_xz_current.append(QPointF(current_point->x, current_point->z));
+//        points_yz_current.append(QPointF(current_point->y, current_point->z));
+//        // Add vectors to series.
+//        fmain::m_series_current_position[fmain::chart_t::XY]->replace(points_xy_current);
+//        fmain::m_series_current_position[fmain::chart_t::XZ]->replace(points_xz_current);
+//        fmain::m_series_current_position[fmain::chart_t::YZ]->replace(points_yz_current);
+//    }
+//    else
+//    {
+//        fmain::m_series_current_position[fmain::chart_t::XY]->clear();
+//        fmain::m_series_current_position[fmain::chart_t::XZ]->clear();
+//        fmain::m_series_current_position[fmain::chart_t::YZ]->clear();
+//    }
 
-    // Update scale.
-    if(!fmain::m_points.empty())
-    {
-        // Determine the center of the plot ranges.
-        double x_avg = (x_min + x_max) / 2.0;
-        double y_avg = (y_min + y_max) / 2.0;
-        double z_avg = (z_min + z_max) / 2.0;
-        // Iterate over charts.
-        for(uint32_t i = 0; i < 3; ++i)
-        {
-            fmain::chart_t chart_type = static_cast<fmain::chart_t>(i);
+//    // Update scale.
+//    if(!fmain::m_points.empty())
+//    {
+//        // Determine the center of the plot ranges.
+//        double x_avg = (x_min + x_max) / 2.0;
+//        double y_avg = (y_min + y_max) / 2.0;
+//        double z_avg = (z_min + z_max) / 2.0;
+//        // Iterate over charts.
+//        for(uint32_t i = 0; i < 3; ++i)
+//        {
+//            fmain::chart_t chart_type = static_cast<fmain::chart_t>(i);
 
-            // Get the chart's current plot area size to figure out aspect ratio.
-            QRectF plot_area = fmain::m_charts[chart_type]->plotArea();
-            double aspect_ratio = plot_area.width() / plot_area.height();
-            // Calculate ranges based on aspect ratio.
-            double xh_min, xh_max, xh_avg, xh_range;
-            double yh_min, yh_max, yh_avg, yh_range;
-            switch(chart_type)
-            {
-                case fmain::chart_t::XY:
-                {
-                    xh_min = x_min;
-                    xh_max = x_max;
-                    yh_min = y_min;
-                    yh_max = y_max;
-                    xh_avg = x_avg;
-                    yh_avg = y_avg;
-                    break;
-                }
-                case fmain::chart_t::XZ:
-                {
-                    xh_min = x_min;
-                    xh_max = x_max;
-                    yh_min = z_min;
-                    yh_max = z_max;
-                    xh_avg = x_avg;
-                    yh_avg = z_avg;
-                    break;
-                }
-                case fmain::chart_t::YZ:
-                {
-                    xh_min = y_min;
-                    xh_max = y_max;
-                    yh_min = z_min;
-                    yh_max = z_max;
-                    xh_avg = y_avg;
-                    yh_avg = z_avg;
-                    break;
-                }
-            }
+//            // Get the chart's current plot area size to figure out aspect ratio.
+//            QRectF plot_area = fmain::m_charts[chart_type]->plotArea();
+//            double aspect_ratio = plot_area.width() / plot_area.height();
+//            // Calculate ranges based on aspect ratio.
+//            double xh_min, xh_max, xh_avg, xh_range;
+//            double yh_min, yh_max, yh_avg, yh_range;
+//            switch(chart_type)
+//            {
+//                case fmain::chart_t::XY:
+//                {
+//                    xh_min = x_min;
+//                    xh_max = x_max;
+//                    yh_min = y_min;
+//                    yh_max = y_max;
+//                    xh_avg = x_avg;
+//                    yh_avg = y_avg;
+//                    break;
+//                }
+//                case fmain::chart_t::XZ:
+//                {
+//                    xh_min = x_min;
+//                    xh_max = x_max;
+//                    yh_min = z_min;
+//                    yh_max = z_max;
+//                    xh_avg = x_avg;
+//                    yh_avg = z_avg;
+//                    break;
+//                }
+//                case fmain::chart_t::YZ:
+//                {
+//                    xh_min = y_min;
+//                    xh_max = y_max;
+//                    yh_min = z_min;
+//                    yh_max = z_max;
+//                    xh_avg = y_avg;
+//                    yh_avg = z_avg;
+//                    break;
+//                }
+//            }
 
-            if(aspect_ratio > 1.0)
-            {
-                yh_range = (yh_max - yh_min) * 1.2;
-                xh_range = yh_range * aspect_ratio;
-            }
-            else
-            {
-                xh_range = (xh_max - xh_min) * 1.2;
-                yh_range = xh_range / aspect_ratio;
-            }
-            // Set axis ranges.
-            fmain::m_axes_x[chart_type]->setRange(xh_avg - xh_range/2.0, xh_avg + xh_range/2.0);
-            fmain::m_axes_y[chart_type]->setRange(yh_avg - yh_range/2.0, yh_avg + yh_range/2.0);
-        }
-    }
+//            if(aspect_ratio > 1.0)
+//            {
+//                yh_range = (yh_max - yh_min) * 1.2;
+//                xh_range = yh_range * aspect_ratio;
+//            }
+//            else
+//            {
+//                xh_range = (xh_max - xh_min) * 1.2;
+//                yh_range = xh_range / aspect_ratio;
+//            }
+//            // Set axis ranges.
+//            fmain::m_axes_x[chart_type]->setRange(xh_avg - xh_range/2.0, xh_avg + xh_range/2.0);
+//            fmain::m_axes_y[chart_type]->setRange(yh_avg - yh_range/2.0, yh_avg + yh_range/2.0);
+//        }
+//    }
 }
-
-// SUBSCRIBERS
-void fmain::subscriber(const sensor_msgs_ext::magnetometerConstPtr &message)
-{
-    // Enforce max data rate.
-    if(fmain::m_point_timer.elapsed() >= 1000.0/fmain::p_max_data_rate)
-    {
-        // Reset point timer.
-        fmain::m_point_timer.restart();
-
-        // Convert message into new point instance.
-        point_t* point = new point_t();
-        point->x = message->x;
-        point->y = message->y;
-        point->z = message->z;
-
-        // Add point to calibration.
-        fmain::m_points.push_back(point);
-
-        // Update charts.
-        fmain::update_charts();
-
-        // Update form's point counter.
-        fmain::ui->lineedit_n_collection_points->setText(QString::number(fmain::m_points.size()));
-    }
-}
-
-
 
 void fmain::on_button_start_fit_clicked()
 {
--- src/magnetometer/gui/fmain.h
+++ src/magnetometer/gui/fmain.h
@@ -7,17 +7,16 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class fmain; }
 QT_END_NAMESPACE
 
+#include "magnetometer/data/data_interface.h"
+
 #include <QtCharts/QChart>
 #include <QtCharts/QValueAxis>
 #include <QtCharts/QScatterSeries>
 #include <QTimer>
-#include <QElapsedTimer>
 
 #include <ros/ros.h>
-#include <sensor_msgs_ext/magnetometer.h>
 
 #include <map>
-#include <deque>
 
 class fmain : public QMainWindow
 {
@@ -47,6 +46,8 @@ private:
     QTimer m_ros_spinner;
     void ros_spin();
 
+    std::shared_ptr<magnetometer::data_interface> m_data_interface;
+
     enum class chart_t
     {
         XY = 0,
@@ -54,17 +55,8 @@ private:
         YZ = 2,
     };
 
-    void start_collection();
-    void stop_collection();
-    void clear_collection();
-
     void start_fit();
     void stop_fit();
-
-    double p_max_data_rate;
-
-    ros::Subscriber m_subscriber;
-    void subscriber(const sensor_msgs_ext::magnetometerConstPtr& message);
 
     enum class state_t
     {
@@ -74,15 +66,6 @@ private:
         TRANSFORM = 3
     };
     state_t m_state;
-
-    struct point_t
-    {
-        double x;
-        double y;
-        double z;
-    };
-    std::deque<point_t*> m_points;
-    QElapsedTimer m_point_timer;
 
     std::map<chart_t, QtCharts::QChart*> m_charts;
     std::map<chart_t, QtCharts::QValueAxis*> m_axes_x;
