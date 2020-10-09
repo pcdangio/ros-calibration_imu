ddf4deedc07b5b0388e830ab2f365a2dba65173e
Paul D'Angio <pcdangio@gmail.com>
2020-10-08 22:19:14 -0400

    magnetometer: updated graph view settings

--- src/magnetometer/graph/graph.cpp
+++ src/magnetometer/graph/graph.cpp
@@ -9,22 +9,32 @@ graph::graph(std::shared_ptr<magnetometer::data_interface>& data_interface)
 
     // Initialize scatter instance.
     graph::m_graph = new QtDataVisualization::Q3DScatter();
+    graph::m_graph->axisX()->setTitle("x");
+    graph::m_graph->axisX()->setTitleVisible(true);
+    graph::m_graph->axisY()->setTitle("z");
+    graph::m_graph->axisY()->setTitleVisible(true);
+    graph::m_graph->axisZ()->setTitle("y");
+    graph::m_graph->axisZ()->setTitleVisible(true);
     graph::m_graph->setAspectRatio(1.0);
     graph::m_graph->setHorizontalAspectRatio(1.0);
+    graph::m_graph->setShadowQuality(QtDataVisualization::QAbstract3DGraph::ShadowQuality::ShadowQualityNone);
 
     // Initialize uncalibrated series.
     graph::m_series_uncalibrated = new QtDataVisualization::QScatter3DSeries();
     graph::m_series_uncalibrated->setBaseColor(QColor(Qt::GlobalColor::blue));
+    graph::m_series_uncalibrated->setItemSize(0.1);
     graph::m_graph->addSeries(graph::m_series_uncalibrated);
 
     // Initialize uncalibrated newest series.
     graph::m_series_uncalibrated_new = new QtDataVisualization::QScatter3DSeries();
     graph::m_series_uncalibrated_new->setBaseColor(QColor(Qt::GlobalColor::red));
+    graph::m_series_uncalibrated_new->setItemSize(0.5);
     graph::m_graph->addSeries(graph::m_series_uncalibrated_new);
 
     // Initialize calibrated series.
     graph::m_series_calibrated = new QtDataVisualization::QScatter3DSeries();
     graph::m_series_calibrated->setBaseColor(QColor(Qt::GlobalColor::green));
+    graph::m_series_calibrated->setItemSize(0.1);
     graph::m_graph->addSeries(graph::m_series_calibrated);
 
     // Initialize flags.
@@ -83,9 +93,6 @@ void graph::update_uncalibrated_plot()
         graph::m_data_interface->get_point(i, point);
         uncalibrated_points->append(point);
     }
-
-    uncalibrated_points->append(QVector3D(-1, 5, 3));
-    uncalibrated_points->append(QVector3D(2, -2, 4));
 
     // Set up newest point if enabled.
     if(graph::f_indicate_new_point && n_points > 0)
